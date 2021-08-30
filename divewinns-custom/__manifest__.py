bd6826e7f725b44aa559e80e264c5b97d4a08fe4
thherman <hermansthibault@gmail.com>
2021-08-30 14:02:54 +0200

    product vendor data

---
+++ divewinns-custom/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "DiveWinns",
+    "author": "istarii",
+    "summary": "DiveWinns customisations",
+    "description": """DiveWinns customisations""",
+    "version": "14.0.1",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_product.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
