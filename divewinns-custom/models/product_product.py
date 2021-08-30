0bd935efad948eacb529942f04373a1394d4342c
thherman <hermansthibault@gmail.com>
2021-08-30 14:58:38 +0200

    product stock by location

--- divewinns-custom/models/product_product.py
+++ divewinns-custom/models/product_product.py
@@ -8,6 +8,9 @@ class ProductProduct(models.Model):
     vendor_product_code = fields.Char(string="Vendor Product Code", compute="_compute_vendor")
     vendor_id = fields.Many2one(comodel_name="res.partner", string="Vendor", compute="_compute_vendor")
 
+    qty_location_32 = fields.Float(string="Qty Divew/Stock", compute="_compute_stock")
+    qty_location_37 = fields.Float(string="Qty Divew/Stock/Reserve", compute="_compute_stock")
+
     def _compute_vendor(self):
         for product in self:
             supplier_info = self.env["product.supplierinfo"].search([("product_id", "=", product.id)], limit=1)
@@ -21,3 +24,16 @@ class ProductProduct(models.Model):
                 product.vendor_id = False
                 product.vendor_product_code = ""
                 product.vendor_product_name = ""
+
+    def _compute_stock(self):
+        for product in self:
+            qty_32 = self.env["stock.quant"].search([("location_id", "=", 32), ("product_id", "=", product.id)])
+            qty_37 = self.env["stock.quant"].search([("location_id", "=", 37), ("product_id", "=", product.id)])
+            if qty_32:
+                product.qty_location_32 = qty_32.quantity
+            else:
+                product.qty_location_32 = 0
+            if qty_37:
+                product.qty_location_37 = qty_37.quantity
+            else:
+                product.qty_location_37 = 0
