ecee857c3002c163bcba42b662d81cffdea68c88
thherman <hermansthibault@gmail.com>
2021-09-04 12:27:41 +0200

    debug

--- divewinns-custom/models/event_event.py
+++ divewinns-custom/models/event_event.py
@@ -4,6 +4,6 @@ class EventEvent(models.Model):
     _name = "event.event"
     _inherit = "event.event"
 
-    training_dates_ids = fields.One2many(comodel_name="divewinns.training.dates", inverse_name="event_id", string="Dates of Training")
+    training_dates_ids = fields.One2many(comodel_name="divewinns.training.date", inverse_name="event_id", string="Dates of Training")
 
     
