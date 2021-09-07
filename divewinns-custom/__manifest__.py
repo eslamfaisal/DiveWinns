d905c56be266c1e4322a5c7790b643d6334b965c
thherman <hermansthibault@gmail.com>
2021-09-07 21:59:46 +0200

    version

--- divewinns-custom/__manifest__.py
+++ divewinns-custom/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "istarii",
     "summary": "DiveWinns customisations",
     "description": """DiveWinns customisations""",
-    "version": "14.0.1",
+    "version": "14.0.2",
     "depends": ["base", "product", "purchase", "stock", "event", "calendar"],
     "data": [
         "security/ir.model.access.csv",
