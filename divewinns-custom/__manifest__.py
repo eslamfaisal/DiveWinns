5772367fb3b6642d8ee83f69f13f442adff4c3c5
thherman <hermansthibault@gmail.com>
2021-10-27 12:31:44 +0200

    debu

--- divewinns-custom/__manifest__.py
+++ divewinns-custom/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "istarii",
     "summary": "DiveWinns customisations",
     "description": """DiveWinns customisations""",
-    "version": "14.0.5",
+    "version": "14.0.6",
     "depends": ["base", "product", "purchase", "stock", "event", "calendar", "hr"],
     "data": [
         "security/ir.model.access.csv",
